1f2506e1bdf0c840e728cc19c63493110541d8c6
rumeone <yuaroslav_29@mail.ru>
2021-09-28 17:02:16 +0700

    Set parse command-line CLI11

--- src/project/main.cpp
+++ src/project/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include "../../include/lib/sumFloat.hpp"
+#include "../../_deps/cli11-src/include/CLI/CLI.hpp"
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+    CLI::App app{"Sum two float"};
 
-	float a = 5.0;
-	float b = 1.3;
+    float first = 0.f;
+    app.add_option("-f, --first", first, "Set first argument")->required();
 
-	cout << sumFloat(a, b) << endl;
+    float second = 0.f;
+    app.add_option("-s, --second", second, "Set second argument")->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+
+	cout << sumFloat(first, second) << endl;
+
+	return 0;
 
 }
